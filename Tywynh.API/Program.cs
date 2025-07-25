00ea17133a1b69d6022ada7017a388cd0b35d898
Rios <riosbunao.dev@gmail.com>
2025-07-25 10:07:02 +0800

    Added CORS policy

--- Tywynh.API/Program.cs
+++ Tywynh.API/Program.cs
@@ -15,6 +15,21 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddFeatures();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:5173",
+                "https://tywynh.vercel.app"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
+
 var app = builder.Build();
 
 // Add this line before other middlewares
@@ -26,6 +41,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 app.MapStoriesEndpoints();
